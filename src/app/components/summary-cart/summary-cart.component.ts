0bb9f2cc804a27ec12386f88918e25efddf95b6d
Konrad Łatwiński <konrad.latwinski@gmail.com>
2024-01-26 10:29:10 +0100

    feat: cart calculate

--- src/app/components/summary-cart/summary-cart.component.ts
+++ src/app/components/summary-cart/summary-cart.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ReadProductDto } from '../../products/ts';
+import { CartService } from '../../services/cart.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-summary-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule],
   templateUrl: './summary-cart.component.html',
   styleUrl: './summary-cart.component.scss'
 })
-export class SummaryCartComponent {
+export class SummaryCartComponent implements OnInit, OnDestroy {
+  cartService = inject(CartService)
+  cartService$ : Subscription = null!
+  cartData : ReadProductDto[] = []
+  formBuilder : FormBuilder = inject(FormBuilder)
+
+  checkoutForm = this.formBuilder.group({
+    email: ''
+  });
+
+  ngOnInit(): void {
+    this.cartService$ = this.cartService.cartData.subscribe(data =>
+       {
+        this.cartData = data;
+       })
+   }
+
+  ngOnDestroy(): void {
+    this.cartService$.unsubscribe();
+  }
+
+  onSubmit(): void  {
+
+    this.checkoutForm.value
+  }
 
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, OnDestroy, inject } from '@angular/core';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { ReadProductDto } from '../products/ts';
+import { CalculateDiscountDto, DiscountsService } from '../discounts/ts';
 @Injectable({
   providedIn: 'root'
 })
-export class CartService {
+export class CartService implements OnDestroy {
   
-  cartData = new BehaviorSubject<Array<ReadProductDto>>([]);
+  cartData = new BehaviorSubject<Array<ReadProductDto>>([])
+  cartDiscount = new BehaviorSubject(0)
 
+  discountsService = inject(DiscountsService)
+  discountsService$ : Subscription = null!;
   constructor() { }
+
 
   addProduct(readProductDto : ReadProductDto)
   {
@@ -24,5 +29,21 @@ export class CartService {
     currentData.splice(indexToRemove, 1); 
     this.cartData.next(currentData); 
   }
+  }
+
+  calculateDiscount(email? : string)
+  {
+    const cartProductsId : string[] = this.cartData.value.map((product) => product.id!)
+    const calculateDiscountDto : CalculateDiscountDto = {
+      productIds: cartProductsId,
+      customerEmail: email
+    }
+    this.discountsService$ = this.discountsService.discountsCalculatePost(calculateDiscountDto).subscribe(response=>
+      {
+        this.cartDiscount.next(response.discount!)
+      })
+  }
+  ngOnDestroy(): void {
+    this.discountsService$?.unsubscribe()
   }
 }
